e61ce885e6b4611af51f3cc6019c80161dfcefe7
Matias Apolonio <matias.apolonio@autodesk.com>
2020-12-13 23:05:01 -0300

    added logging of runtime

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const run = async () => {
   const { main } = require(`./day${day}/day${day}-part-${part}`);
 
   console.log(`Running Day ${day} - Part ${part}\n`);
+  console.time('Runtime');
 
   const result = await main();
 
   console.log(`Result: ${result}`);
+  console.timeEnd('Runtime');
 };
 
 run();
